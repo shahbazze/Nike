83a0114dfeacd1b384fbf9e9ec7de70a500cd58b
shahbazze <148189830+shahbazze@users.noreply.github.com>
2024-07-05 09:59:07 +0500

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,24 +1,52 @@
-import {headerLogo} from "../assets/images"
-import {hamburger} from "../assets/icons"
-import {navLinks} from "../constants"
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
+import { useEffect, useState } from "react";
 const Nav = () => {
-  return (
-    <header className="padding-x py-8 absolute z-10 w-full">
-        <nav className="flex justify-between items-center max-container">
-            <a href="/"><img src={headerLogo} alt="logo" width={130} height={29} /></a>
-            <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
-                {
-                    navLinks.map((item)=>(
-                        <li key={item.label}><a href={item.href} className="fonts-montserrat leading-normal text-lg text-slate-gray">{item.label}</a></li>
-                    ))
-                }
-            </ul>
-            <div className="hidden max-lg:block">
-                <img src={hamburger} alt="hamburger" width={25} height={25} />
-            </div>
-        </nav>
-    </header>
-  )
-}
+  const [show, setshow] = useState(false);
 
-export default Nav
+  const handleClick = () => {
+    if(show)
+        return setshow(false);
+    return setshow(true);
+  };
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full ">
+      <nav
+        className="flex justify-between
+        lg:items-center max-container"
+      >
+        <a href="/">
+          <img
+            src={headerLogo}
+            alt="logo"
+            width={130}
+            height={29}
+            className="max-lg:top-0"
+          />
+        </a>
+        <ul
+          className={`flex-1 flex justify-center items-center
+             gap-16 max-lg:flex-col max-lg:gap-4
+             max-lg:items-end max-lg:mt-10 ${show ?"":"max-lg:hidden"}`}
+        >
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="fonts-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="hidden max-lg:block" onClick={handleClick}>
+          <img src={hamburger} alt="hamburger" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
